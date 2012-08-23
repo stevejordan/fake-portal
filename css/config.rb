2a73d72059d875f0ebf48f16936bbd97624fed34
Steve Jordan <sjordan@ntlworld.com>
2012-08-23 19:39:45 +0100

    removing foundation including fakeportal as single file including js libs more css changes to come

--- css/config.rb
+++ css/config.rb
@@ -1,4 +1,4 @@
-require 'zurb-foundation'
+#require 'zurb-foundation'
 # Require any additional compass plugins here.
 
 
@@ -10,7 +10,7 @@ images_dir = "../images"
 javascripts_dir = "../js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed #:expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
