8b92ec7df6d516ee13a72a2b52011df227772568
Steve Jordan <sjordan@ntlworld.com>
2012-10-17 16:48:36 +0100

    person template now fuller foundicons included sass files now tracked

--- js/views/person.js
+++ js/views/person.js
@@ -1,7 +1,7 @@
 define([
     'underscore', 'backbone', 'models/person', 'namespace', 'helpers'
 ], function ( _, Backbone, PersonModel, ns, Helpers) {
-           
+
     var PersonView = Backbone.View.extend({
 
         //hook into section#about
@@ -12,8 +12,8 @@ define([
 
         //slogan of the week
         welcomeSlogans: [
-          'Good to see you',
-          'I like you the best'
+            'Good to see you',
+            'I like you the best'
         ],
 
         events: {
@@ -27,17 +27,29 @@ define([
             this.render();
         },
 
-        render: function() {
+        render: function () {
+
+            var modelJson = this.model.toJSON();
+
+            if (modelJson.udbschoolid) {
+                modelJson.udbschoolid = 
+                    this.convertUdbString(modelJson.udbschoolid);
+            }
+
+            if (modelJson.udbdepartmentid) {
+                modelJson.udbdepartmentid =
+                    this.convertUdbString(modelJson.udbdepartmentid);
+            }
 
             Helpers.debug('rendering the person details...');
             this.insertPoint.html(
-                this.template(this.model.toJSON())
+                this.template(modelJson)
             );
 
             this.welcomeBar.html(
                 this.templateWelcomeBar(
                     _.extend(
-                        {'slogan': this.getSlogan()}, 
+                        {'slogan': this.getSlogan()},
                         this.model.toJSON()
                     )
                 )
@@ -45,12 +57,12 @@ define([
 
         },
 
-        openface: function() {
+        openface: function () {
             Helpers.debug('click on face!');
         },
 
         //returns a random slogan
-        getSlogan: function() {
+        getSlogan: function () {
             var random = (function (max) {
 
                 return Math.floor(Math.random() * (max));
@@ -58,6 +70,49 @@ define([
             }(this.welcomeSlogans.length));
 
             return this.welcomeSlogans[random];
+
+        },
+
+        //converts udb school/dept into readable text
+        convertUdbString: function (udbString) {
+
+            var map = {
+                SSSOSS: "School of Social Sciences",
+                SSECON: "Department of Economics",
+                SSIPOL: "Department of International Politics",
+                SSSOCL: "Department of Sociology",
+                BBCASS: "Cass Business School",
+                BBUGPR: "Undergraduate Programmes",
+                AAJOUR: "Journalism and Publishing",
+                IICOMP: "School of Informatics",
+                EESEMS: "School of Engineering and Mathematical Sciences",
+                HHIOHS: "School of Health Sciences",
+                HNHCED: "Education Development Unit",
+                HNABBS: "Applied Biological Sciences",
+                AAARTP: "Cultural Policy and Management",
+                AASOAR: "School of Arts",
+                HAOPTM: "Optometry",
+                HNAPSS: "Interdisciplinary Studies in Professional Practice",
+                AAMUSC: "Music",
+                SSPSYC: "Department of Psychology",
+                LLILAW: "The City Law School",
+                HARADI: "Radiography",
+                AALANG: "Centre for Translation Studies",
+                HNMWIF: "Maternal and Child Health",
+                HNADUN: "Adult Nursing",
+                HNMHLD: "Mental Health and Learning Disabilities",
+                HPCIPS: "Investigative, Security and Police Sciences",
+                HACOMS: "Language and Communication Science",
+                UALDCN: "Learning Development Centre",
+                UUCITY: "Central Services"
+            };
+
+            if (map[udbString]) {
+                return map[udbString];
+            }
+
+            //we failed to find a match
+            return false;
 
         }
 
