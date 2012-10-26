65ed862d608595238cb0d12d97313e5f98b06f0e
Steve Jordan <sjordan@ntlworld.com>
2012-10-26 10:33:01 +0100

    moving window init stuff into main kickoff script

--- js/fakeportal.js
+++ js/fakeportal.js
@@ -1,11 +1,3 @@
-/*jslint
- evil: true, browser: true, nomen: true, plusplus: true
-*/
-
-/*global
- jQuery, $, Backbone, _
-*/
-
 require.config({
     paths: {
         jquery: 'lib/jquery.min',
@@ -26,7 +18,7 @@ require.config({
             exports: 'Backbone'
         },
         'foundation': {
-            deps: [ 
+            deps: [
                 'jquery',
                 'vendor/foundation/modernizr.foundation',
                 'vendor/foundation/jquery.placeholder',
@@ -46,27 +38,84 @@ require.config({
 
 require([
     'views/fakeportal', 'namespace', 'foundation'
-], function ( FakePortal, ns ) {
+], function (FakePortal, ns) {
+
+    'use strict';
+
+    var $windowObj = $(window),
+        windowWidth,
+        $navToggler = $('#global-nav-toggler'),
+        $globalNav = $('#global-nav1'),
+        /* resize event handlers */
+        didResize = false,
+        breakpoints = {
+            'tabletPortrait' : 768,
+            'phonePortrait' : 480
+        };
 
     //global jquery ajax error handler
-     $("#working").bind("ajaxSend", function(){
-         $(this).show();
-     }).bind("ajaxComplete", function(){
-         $(this).hide();
-     });
+    $("#working").bind("ajaxSend", function () {
+        $(this).show();
+    }).bind("ajaxComplete", function () {
+        $(this).hide();
+    });
 
-     $("#error").bind("ajaxError", function(e, error){
-         $(this)
+    $("#error").bind("ajaxError", function (e, error) {
+        $(this)
              .html('problem with ajax request for : ' + error.status + ' ' + error.statusText +
                   (error.status === 403 ? '<br/>need to sign into <a href="https://www.dev.city.ac.uk/portal-poc/remote-services" target="new">www.dev</a>?' : ''))
              .show()
-             .click(function () {$(this).hide();});
-         
-     });
+             .click(function () {$(this).hide(); });
+
+    });
+
+
+    function getWindowWidth() {
+        var wWidth =  Math.round($windowObj.width());
+        return wWidth;
+    }
+
+    function windowResized(windowWidth) {
+
+        if (windowWidth >= breakpoints.tabletPortrait) {
+            $navToggler.removeClass("active");
+            //clear any inline styles
+            $globalNav.removeAttr("style");
+        }
+
+    }
+
+    function pageInit() {
+
+        $navToggler.bind('click', function () {
+            $navToggler.toggleClass('active');
+            $globalNav.slideToggle('slow', function () {
+                $globalNav.toggleClass('active');
+            });
+        });
+    }//pageInit
+
+    windowWidth = getWindowWidth();
+
+    $windowObj.resize(function () {
+        didResize = true;
+    });
+
+    setInterval(function () {
+        if (didResize) {
+            didResize = false;
+            var newWindowWidth = getWindowWidth();
+
+            if (newWindowWidth !== windowWidth) {
+                windowWidth = newWindowWidth;
+                windowResized(windowWidth);
+            }
+        }
+    }, 250); // setInterval
+
+    pageInit();
 
     // this is the main view that kicks it all off
-    var main = new FakePortal();
-
-    ns.FakePortal = main;
+    ns.FakePortal = new FakePortal();
 
 });
